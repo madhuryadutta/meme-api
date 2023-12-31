70d5dbd910df479613cd9ccf447c5fe43dbe8d94
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2023-12-31 16:35:04 +0530

    Flyctl Config

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.staticfiles import StaticFiles
 
 import os
 import random
+from dotenv import load_dotenv
 
 app = FastAPI()
 
@@ -15,17 +16,21 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 source_list=[]
+
 def load_data():
     global source_list
     path = "./local/meme"
     source_list = os.listdir(path)
     print('Dataset Loaded')
+
 load_data()
+load_dotenv()
+CDN=os.getenv('DB_URL_ENV')
 
 app.mount("/static", StaticFiles(directory="local/meme"), name="static")
 
 @app.get("/")
 def read_root(request: Request):
     client_host = request.client.host
-    return {"random_meme":  '/static/'+random.choice(source_list)}
+    return {"random_meme":  'http://' + str(CDN)+'/static/'+random.choice(source_list)}
 
