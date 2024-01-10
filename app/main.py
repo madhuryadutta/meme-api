7af315bcd7ff6197e6e73043d475f3133607cc1e
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2024-01-10 17:48:04 +0000

    CORS header are added

--- app/main.py
+++ app/main.py
@@ -3,12 +3,22 @@ from typing import Union
 from fastapi import FastAPI ,Request
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 import os
 import random
 from dotenv import load_dotenv
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Item(BaseModel):
     name: str
